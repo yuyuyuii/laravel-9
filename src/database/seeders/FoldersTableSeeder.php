f205f5626d1130b8cc0f1ee35fc9a0491ff5b4d2
y.maeda <yuyuyu.mae606@gmail.com>
2022-03-26 19:10:30 +0900

    テストデータ登録

---
+++ src/database/seeders/FoldersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Model;
+
+class FoldersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $titles = ['プライベート', '仕事', '旅行'];
+      foreach($titles as $title){
+        DB::table('folders')->insert([
+          'title' => $title,
+          'created_at' => Carbon::now(),
+          'updated_at' => Carbon::now(),
+        ]);
+      }
+    }
+}
